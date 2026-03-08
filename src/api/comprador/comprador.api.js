83118a6c1892f4208b9fc66b18a8799bd3eb4f11
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-03-07 23:01:48 -0500

    API para el manejo de información de los compradores

---
+++ src/api/comprador/comprador.api.js
@@ -0,0 +1,39 @@
+import { instance } from '../base.api'
+
+const model = 'personas'
+
+const compradorAPI = {
+  listarTodos: (token) => {
+    return instance.get(`/${model}/compradores/todos`, {
+      headers: {
+        'x-token': token,
+      },
+    })
+  },
+
+  agregarProductor: (data, token) => {
+    return instance.post(`/${model}/compradores/agregar`, data, {
+      headers: {
+        'x-token': token,
+      },
+    })
+  },
+
+  actualizarProductor: (id, data, token) => {
+    return instance.patch(`/${model}/compradores/actualizar-informacion/${id}`, data, {
+      headers: {
+        'x-token': token,
+      },
+    })
+  },
+
+  eliminarProductor: (id, token) => {
+    return instance.delete(`/${model}/compradores/borrar-persona/${id}`, {
+      headers: {
+        'x-token': token,
+      },
+    })
+  },
+}
+
+export default compradorAPI
--- src/api/index.api.js
+++ src/api/index.api.js
@@ -3,4 +3,5 @@ import usuarioAPI from './usuario/usuario.api'
 import empresaAPI from './empresa/empresa.api'
 import productoAPI from './producto/producto.api'
 import productorAPI from './productor/productor.api'
-export { authAPI, usuarioAPI, empresaAPI, productoAPI, productorAPI }
+import compradorAPI from './comprador/comprador.api'
+export { authAPI, usuarioAPI, empresaAPI, productoAPI, productorAPI, compradorAPI }
