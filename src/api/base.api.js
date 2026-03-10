0ead910ec8174b8fb782b745d92ee18d4ab56cf1
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-03-10 12:11:13 -0500

    Baseurl modificada

--- src/api/base.api.js
+++ src/api/base.api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { server } from '../config/index.config'
 export const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/aroma-de-oro',
+  baseURL: `${server}`,
 })
